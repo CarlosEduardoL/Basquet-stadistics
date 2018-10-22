6ea32004d39a85925350f4d4f45f4e158536c4c2
CarlosEduardoL <carlos.lizalda1@correo.icesi.edu.co>
2018-10-22 18:52:25 -0500

    dash added

--- src/utilites/PlayerGeneratos.java
+++ src/utilites/PlayerGeneratos.java
@@ -29,14 +29,6 @@ public class PlayerGeneratos {
 			alv += temp + "separator";
 		}
 		reader.close();
-		String players[] = alv.split("separator");
-		for (int i = 0; i < players.length; i++) {
-			String data[] = players[i].split(",");
-			String ab = data[2] + "," + data[0] + "," + data[1] + "," + data[3] + "," + data[5] + ","
-					+ data[7] + "," +  data[9] + "," +  data[42] + "," +  data[54] + "," +  data[88] +
-					"," +  data[105] + "," +  data[106] + "\n";
-			writer.write(ab);
-		}
 		writer.close();
 		
 	}
