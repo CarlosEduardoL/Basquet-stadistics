1ca17e0ef1267aeb857499b39159d5a1460e9a8b
Carlos Eduardo Lizalda Valencia <1006425608@academico.icesi.edu.co>
2018-10-23 15:54:15 -0500

    Player generator an base data completed

---
+++ src/trees/binarySearchTree/BSNode.java
@@ -0,0 +1,85 @@
+package trees.binarySearchTree;
+
+public class BSNode<K extends Comparable<K>, V> {
+	
+	private BSNode<K, V> left;
+	
+	private BSNode<K, V> rigth;
+	
+	private BSNode<K, V> parent;
+	
+	private V value;
+	
+	private K key;
+	
+	public BSNode(K key,V value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public BSNode<K, V> getLeft() {
+		return left;
+	}
+
+	public void setLeft(BSNode<K, V> left) {
+		this.left = left;
+	}
+
+	public BSNode<K, V> getRigth() {
+		return rigth;
+	}
+
+	public void setRigth(BSNode<K, V> rigth) {
+		this.rigth = rigth;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public BSNode<K, V> getParent() {
+		return parent;
+	}
+
+	public void setParent(BSNode<K, V> patern) {
+		this.parent = patern;
+	}
+	
+	public boolean haveLeft()
+    {
+        return left != null;
+    }
+
+    public boolean haveRight()
+    {
+        return rigth != null;
+    }
+
+    public boolean isTheRoot()
+    {
+        return parent == null;
+    }
+
+    public boolean isRightSon()
+    {
+        return parent != null && parent.getRigth() != null && parent.getRigth().equals(this);
+    }
+
+    public boolean isleftSon()
+    {
+        return parent != null && parent.getLeft() != null && parent.getLeft().equals(this);
+    }
+    
+    public boolean isLeaf() {
+    	return !(haveLeft() || haveRight());
+    }
+
+}
--- src/utilites/PlayerGeneratos.java
+++ src/utilites/PlayerGeneratos.java
@@ -21,15 +21,32 @@ public class PlayerGeneratos {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
+		File location = new File("playersData/");
+		location.mkdirs();
+		System.out.println(location.getAbsolutePath());
 		BufferedReader reader = new BufferedReader(new FileReader(new File("rubros.csv"))); 
-		BufferedWriter writer = new BufferedWriter(new FileWriter(new File("MySelectedData.csv")));
+		
 		String temp;
 		String alv = "";
 		while((temp = reader.readLine())!=null) {
-			alv += temp + "separator";
+			alv += temp + "separatorPro2017Lol";
 		}
+		String players[] = alv.split("separatorPro2017Lol");
+		
+		for (int i = 0; i < players.length; i++) {
+			
+			location = new File("playersData/player_"+i);
+			BufferedWriter writer = new BufferedWriter(new FileWriter(location));
+			String[] rubros = players[i].split(",");
+			String player = "";
+			for (int j = 0; j < rubros.length; j++) {
+				player += rubros[j] + "\n";			
+			}
+			writer.write(player);
+			writer.close();
+		}
+		
 		reader.close();
-		writer.close();
 		
 	}
 
